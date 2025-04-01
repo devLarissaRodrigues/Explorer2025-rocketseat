ad555b80abd02d2f9111e4606ba209eae7aaf97b
devLarissaRodrigues <larissa.rodrigues@ccc.ufcg.edu.br>
2025-03-31 22:57:42 -0300

    Database initialization

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 require("express-async-errors");
+const database = require("./database/sqlite");
 const AppError = require("./utils/AppError");
-
 const express = require("express");
-
 const routes = require("./routes");
 
 const app = express();
 app.use(express.json());
 
+
 app.use(routes);
+
+database();
+
 app.use((error, request, response, next) => {
     if(error instanceof AppError){
         return response.status(error.statusCode).json({
@@ -22,8 +25,6 @@ app.use((error, request, response, next) => {
         status: "error",
         message: "Internal server error",
      })
-
-
 });
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
