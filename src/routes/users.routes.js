e5e2889e27140f9659cf563e4131b9cfed0d486b
devLarissaRodrigues <larissa.rodrigues@ccc.ufcg.edu.br>
2025-04-03 23:57:45 -0300

    Creating notesController

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,9 +2,9 @@ const { Router } = require("express");
 const UsersController = require("../controllers/UsersController");
 const usersRoutes = Router();
 
-const usersControler = new UsersController();
+const usersController = new UsersController();
 
-usersRoutes.post("/", usersControler.create);
-usersRoutes.put("/:id", usersControler.update);
+usersRoutes.post("/", usersController.create);
+usersRoutes.put("/:id", usersController.update);
 
 module.exports = usersRoutes;
