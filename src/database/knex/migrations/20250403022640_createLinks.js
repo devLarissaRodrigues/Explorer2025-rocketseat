10873ff847ff90e28feada3be39145a030be3746
devLarissaRodrigues <larissa.rodrigues@ccc.ufcg.edu.br>
2025-04-05 13:28:10 -0300

    CRUD notes

--- src/database/knex/migrations/20250403022640_createLinks.js
+++ src/database/knex/migrations/20250403022640_createLinks.js
@@ -3,7 +3,7 @@ exports.up = knex => knex.schema.createTable("links", table => {
     table.text("url").notNullable;
     table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE");
 
-    table.timestamp("created_at").default(knex.fn.now);
+    table.timestamp("created_at").defaultTo(knex.fn.now());
 });
 
 exports.down = knex => knex.schema.dropTable("links");
